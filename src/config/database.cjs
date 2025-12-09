65293fe23b5db2244062821db9d3ae49c17d74df
artem <artsiomharminovich05@gmail.com>
2025-12-09 17:58:12 +0200

    fix: hm 5

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,12 +1,16 @@
-require("dotenv").config();
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres"
-  }
+    development: {
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || null,
+        database: process.env.DB_NAME || 'Node',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+        dialect: 'postgres',
+        logging: false
+    }
 };
